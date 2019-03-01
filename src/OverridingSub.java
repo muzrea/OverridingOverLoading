a90e30098f56741653dbea592382df6c96042086
muzrea <muzrea@hotmail.com>
2019-03-01 19:49:28 +0800

    create OverridingSub class

---
+++ src/OverridingSub.java
@@ -0,0 +1,9 @@
+/**
+ * Created by 苏晓风 on 2019/3/1.
+ */
+public class OverridingSub extends OverridingUsage{
+    @Override
+    public void eat() {
+        System.out.println("Children can eat.");
+    }
+}
