e6f38a966e735df1813c1920572bdd27b7dbe3ea
muzrea <muzrea@hotmail.com>
2019-03-01 19:51:00 +0800

    create Overloading class

---
+++ src/OverloadingUsage.java
@@ -0,0 +1,11 @@
+/**
+ * Created by 苏晓风 on 2019/3/1.
+ */
+public class OverloadingUsage {
+    public void run(){
+        System.out.println("I can run.");
+    }
+    public void run(String name){
+        System.out.println(name+"can run.");
+    }
+}
